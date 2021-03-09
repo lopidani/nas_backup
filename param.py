49d919f6b4724dfc8e6fecd980d3bc94077202dc
Lopi <50907836+lopidani@users.noreply.github.com>
2021-03-09 12:53:47 +0200

    Add files via upload

---
+++ param.py
@@ -0,0 +1,40 @@
+#-------------------------------
+# file server IP
+nas_ip='192.168.30.100'
+
+#-------------------------------
+# nas ssh parameters
+ssh_port=22
+ssh_user="root"
+ssh_passw="netgear1"
+
+#------------------------------
+# ftpsync parameters
+# if we set ftp user login on NAS
+ftp_user="itc"
+ftp_passw="75493"
+# user anonymus
+# if we set ftp anonymus login on NAS
+#ftp_user=""
+#ftp_passw=""
+
+#------------------------------
+# frecventa upload-ului in zile (integer)
+# daca nu o vrem in program o luam de aici
+#fr=7
+
+#------------------------------
+# folder where is stored user who run code
+# and the code 
+#user_file="C:\\Users\\admin\\AppData\\Local\\Nas_backup\\user.txt" 
+#log_file="C:\\Users\\admin\\AppData\\Local\\Nas_backup\\backup_errors.txt"
+
+#-------------------------------
+# pc ssh parameters
+#import socket 
+#host_name = socket.gethostname() 
+#pc_ip = socket.gethostbyname(host_name)
+#port_pc=22
+#user_pc="admin"
+#pass_pc="H4n1b477@GSP" 
+#backup_file="C:\\Users\\admin\\AppData\\Local\\Nas_backup\\backup.py" 
